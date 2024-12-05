de6410c30a646929439a8586c1e7b341c55fc85d
Dhruv Rajpoot <dhruv.r@hyperverge.co>
2024-12-05 11:28:44 +0530

    fix: movieinfocontainer

--- src/components/MovieInfoContainer/MovieInfoContainer.tsx
+++ src/components/MovieInfoContainer/MovieInfoContainer.tsx
@@ -2,34 +2,44 @@ import React from "react";
 import { LeftArrow, Heart } from "../Icons/icon";
 import VideoBg from "../VideoBg/VideoBg";
 import MovieInfo from "../MovieInfo/MovieInfo";
+import Slider from "../Slider/Slider";
+import Description from "../Description/Description";
+import HeadingContainer from "../HeadingContainer/HeadingContainer";
+
+export interface Movie {
+  _id: string;
+  image: string;
+  title: string;
+  productionHouse: string;
+  rating: number;
+  imdb: number;
+  description: string;
+  imagesInTheMovie: {
+    link: string;
+    image: string;
+  }[];
+  videoLink: string;
+  videoThumbnail: string;
+  releaseDate: string;
+}
 
 interface MovieInfoContainerProps {
   onBackClick: () => void;
   onHeartClick: () => void;
-  videoLink: string;
-  videoThumbnail: string;
   isFavorite?: boolean;
-  movieInfo: {
-    image: string;
-    title: string;
-    productionHouse: string;
-    rating: number;
-    imdb: number;
-  };
+  movieInfo: Movie;
 }
 
 const MovieInfoContainer: React.FC<MovieInfoContainerProps> = ({
   onBackClick,
   onHeartClick,
   isFavorite = false,
-  videoLink,
-  videoThumbnail,
   movieInfo,
 }) => {
   return (
     <div>
       <div className="relative">
-        <VideoBg thumbnail={videoThumbnail} link={videoLink} />
+        <VideoBg thumbnail={movieInfo?.videoThumbnail} link={movieInfo?.videoLink} />
 
         <div className="absolute left-0 bottom-0 w-full h-1/3 bg-gradient-to-t from-black via-[#0000009f] to-transparent" />
 
@@ -56,6 +66,20 @@ const MovieInfoContainer: React.FC<MovieInfoContainerProps> = ({
 
       <div className="flex flex-col gap-7 px-5 pb-7">
         <MovieInfo data={movieInfo} />
+
+        <div className="flex flex-col gap-4">
+          <Description
+            data={movieInfo?.description || "No description available."}
+          />
+          <HeadingContainer label="Images in the movie">
+            {movieInfo?.imagesInTheMovie &&
+            movieInfo?.imagesInTheMovie.length > 0 ? (
+              <Slider data={movieInfo?.imagesInTheMovie || []} />
+            ) : (
+              <div>No images available for this movie.</div>
+            )}
+          </HeadingContainer>
+        </div>
       </div>
     </div>
   );
